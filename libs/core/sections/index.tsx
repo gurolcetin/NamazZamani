f65eb326ef96ab5b8cecfe09377d540078ca2d9d
Gürol Mehmet Çetin <gurolmehmetcetin@gmail.com>
2024-03-04 22:04:27 +0300

    Fix test script and update iPhone simulator command

--- libs/core/sections/index.tsx
+++ libs/core/sections/index.tsx
@@ -1,4 +1,4 @@
-import FastingForm from './MissedTracking/Fsating/FsatingForm';
+import FastingForm from './MissedTracking/Fsating/FastingForm';
 import CalculatedMissedPrayer from './MissedTracking/Prayer/CalculatedMissedPrayer';
 import PrayerForm from './MissedTracking/Prayer/PrayerForm';
 import CalculatedMissedFasting from './MissedTracking/Fsating/CalculatedMissedFasting';
