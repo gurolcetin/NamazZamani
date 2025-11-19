f5fc170f623adc2fa7b3a7f7850277e22fba45db
Gürol Mehmet Çetin <gurolmehmetcetin@gmail.com>
2025-11-20 00:17:42 +0300

    back button style changes

--- src/navigation/Stack/PrayerTimeStack.tsx
+++ src/navigation/Stack/PrayerTimeStack.tsx
@@ -7,62 +7,57 @@ import { useTheme } from '../../../libs/core/providers';
 import TimeTable from '../../screens/PrayerTime/time-table/time-table';
 import QiblaScreen from '../../screens/PrayerTime/qibla/qibla-finder';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { BackButton } from '../../../libs/components';
 
 const Stack = createNativeStackNavigator();
 
 export const PrayerTimeStack = () => {
   const { currentTheme } = useTheme();
+
   return (
-    <Stack.Navigator initialRouteName={Routes.PrayerTime}>
+    <Stack.Navigator
+      initialRouteName={Routes.PrayerTime}
+      screenOptions={({ navigation }) => ({
+        headerStyle: {
+          backgroundColor: currentTheme.backgroundColor,
+        },
+        headerTitle: '',
+        headerShadowVisible: false,
+        contentStyle: {
+          paddingTop: 20,
+        },
+        headerLeft: ({ canGoBack }) =>
+          canGoBack ? (
+            <BackButton
+              onPress={navigation.goBack}
+              backgroundColor={currentTheme.cardViewBackgroundColor}
+              iconColor={currentTheme.textColor}
+            />
+          ) : null,
+      })}
+    >
       <Stack.Screen
         name={Routes.PrayerTime}
         component={PrayerTime}
-        options={{ headerShown: false, headerLargeTitle: true }}
+        options={{
+          headerShown: false, // sadece bu ekranda header yok
+          contentStyle: undefined, // istersen paddingTop burada sıfırlayabilirsin
+        }}
       />
+
       <Stack.Screen
         name={PrayerTimeScreens.LocationSelector}
         component={LocationSelector}
-        options={{
-          headerStyle: {
-            backgroundColor: currentTheme.statusBarColor,
-          },
-          headerTitle: '',
-          gestureEnabled: true,
-        }}
       />
+
       <Stack.Screen
         name={PrayerTimeScreens.MontlyCalendar}
         component={MonthlyCalendar}
-        options={{
-          headerStyle: {
-            backgroundColor: currentTheme.statusBarColor,
-          },
-          headerTitle: '',
-          gestureEnabled: true,
-        }}
       />
-      <Stack.Screen
-        name={PrayerTimeScreens.Imsakiye}
-        component={TimeTable}
-        options={{
-          headerStyle: {
-            backgroundColor: currentTheme.statusBarColor,
-          },
-          headerTitle: '',
-          gestureEnabled: true,
-        }}
-      />
-      <Stack.Screen
-        name={PrayerTimeScreens.Qibla}
-        component={QiblaScreen}
-        options={{
-          headerStyle: {
-            backgroundColor: currentTheme.statusBarColor,
-          },
-          headerTitle: '',
-          gestureEnabled: true,
-        }}
-      />
+
+      <Stack.Screen name={PrayerTimeScreens.Imsakiye} component={TimeTable} />
+
+      <Stack.Screen name={PrayerTimeScreens.Qibla} component={QiblaScreen} />
     </Stack.Navigator>
   );
 };
